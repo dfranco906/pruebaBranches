c763776d1bd5d44a029bd2c61c6bf5ff3811157b
Thiago <thiagoamarilla780@gmail.com>
2026-04-16 15:02:32 -0300

    zarateGay

---
+++ viruspito.py
@@ -0,0 +1,6 @@
+hola soy un virus  que  crea pitos y te  siguen, es super divertido broooo
+also si quieres que te siga un pito, solo tienes que decir "sigue pito" y el pito te seguira a todas partes, incluso al baño jajaja
+pero cuidado, si dices "deja de seguirme pito" el pito se ira y no te seguira mas, asi que piensa bien antes de decirlo broooo
+ademas, si quieres que el pito te haga cosquillas, solo tienes que decir "hazme cosquillas pito" y el pito te hara cosquillas en los pies, es super divertido broooo
+pero si quieres que el pito te deje de hacer cosquillas, solo tienes que decir "deja de hacerme cosquillas pito" y el pito dejara de hacerte cosquillas, asi que ten cuidado con lo que dices broooo
+en resumen, este virus es super divertido y te hara reir mucho con sus pitos, pero tambien
